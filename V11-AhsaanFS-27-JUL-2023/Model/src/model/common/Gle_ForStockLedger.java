7175a84ff30377d5f4491b561b3482a6412881b7
Muneeb Rehman <61393958+Mabbii@users.noreply.github.com>
2026-04-09 10:29:12 +0500

    Delete V11-AhsaanFS-27-JUL-2023 directory

--- V11-AhsaanFS-27-JUL-2023/Model/src/model/common/Gle_ForStockLedger.java
+++
@@ -1,9 +0,0 @@
-package model.common;
-
-import java.sql.Timestamp;
-
-import oracle.jbo.ViewObject;
-
-public interface Gle_ForStockLedger extends ViewObject {
-    void refreshQuery(String coaName, Timestamp startDate, Timestamp endDate);
-}
